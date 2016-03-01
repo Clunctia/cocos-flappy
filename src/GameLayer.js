7b616a4a81804994f2e30423ac85efe699dd2c70
Goofy <goofy_clunctia@hotmail.com>
2016-03-01 13:46:34 +0900

    fix some buggi

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -1,4 +1,4 @@
 this.player = new Player();
-        this.player.setPosition( new cc.Point( screenWidth / 2, screenHeight / 2 ) );
-        this.addChild( this.player );
-        this.player.scheduleUpdate();
+this.player.setPosition( new cc.Point( screenWidth / 2, screenHeight / 2 ) );
+this.addChild( this.player );
+this.player.scheduleUpdate();
